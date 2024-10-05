be48cdcc7e0d48853123855275bd0e1fbc8b4a45
信萌新 <1273730703@qq.com>
2024-10-06 04:49:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -107,7 +107,7 @@ elif [[ "$OS_TYPE" == "Linux" ]]; then
     if [[ $os_type == "ubuntu" ]]; then
         sudo sed -i.bak -r 's|^#?(deb|deb-src) http://archive.ubuntu.com/ubuntu/|\1 https://mirrors.ustc.edu.cn/ubuntu/|' /etc/apt/sources.list
         sudo apt update && sudo apt upgrade -y
-        sudo apt install -y openssh-server net-tools git unzip fzf ninja-build neovim ruby-full cmake nodejs iputils-ping procps htop traceroute tree coreutils zsh fontconfig
+        sudo apt install -y openssh-server net-tools git unzip fzf ninja-build neovim ruby-full cmake nodejs iputils-ping procps htop traceroute tree coreutils zsh fontconfig python3
     elif [[ $os_type == "fedora" ]]; then
         sudo sed -e 's|^metalink=|#metalink=|g' \
                  -e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.ustc.edu.cn/fedora|g' \
@@ -116,7 +116,7 @@ elif [[ "$OS_TYPE" == "Linux" ]]; then
                  /etc/yum.repos.d/fedora-updates.repo
          
         sudo dnf makecache
-        sudo dnf update -y && sudo dnf install -y openssh-server iproute net-tools fd-find git unzip ripgrep fzf ninja-build neovim ruby kitty cmake nodejs iputils procps-ng htop traceroute fastfetch tree coreutils zsh fontconfig
+        sudo dnf update -y && sudo dnf install -y openssh-server iproute net-tools fd-find git unzip ripgrep fzf ninja-build neovim ruby kitty cmake nodejs iputils procps-ng htop traceroute fastfetch tree coreutils zsh fontconfig python3
         sudo dnf group install -y "C Development Tools and Libraries"
         sudo dnf clean all
     else

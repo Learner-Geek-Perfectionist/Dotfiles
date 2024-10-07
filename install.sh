b255feed4e156ff8ebfa6f7787b94a1a4d18e417
信萌新 <1273730703@qq.com>
2024-10-07 17:04:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -213,8 +213,8 @@ Dotfiles_REPO_URL="https://github.com/Learner-Geek-Perfectionist/dotfiles/archiv
 Fonts_REPO_URL="https://github.com/Learner-Geek-Perfectionist/Fonts/archive/refs/heads/master.zip"
 
 # 定义文件和目标目录名称
-zip_Fonts_file="Fonts_master.zip"
-zip_Dotfiles_file="Dotfiles_master.zip"
+zip_Fonts_file="Fonts-master.zip"
+zip_Dotfiles_file="Dotfiles-master.zip"
 
 dest_Fonts="Fonts_master"
 dest_Dotfiles="Dotfiles_master"

f2d8c794575d06c37c35cb3336bbefadd1f49de2
信萌新 <1273730703@qq.com>
2024-10-10 00:09:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -126,7 +126,7 @@ if [[ $OS_TYPE == "Darwin" ]]; then
     ca-certificates icu4c luajit node unibilium
     cmake libnghttp2 luv openssl@3 vim
     cmake-docs libsodium lz4 pcre2 xz
-    fastfetch libuv lzip python z3
+    fastfetch libuv lzip z3
     fd libvterm make readline zstd
     fzf libyaml mpdecimal ripgrep
     gcc ninja wget mas pkg-config

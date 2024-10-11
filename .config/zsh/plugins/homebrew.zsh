2741627e9590208a0757f5e2c5eb73647e3f6d29
信萌新 <1273730703@qq.com>
2024-10-11 22:51:22 +0800

    Update homebrew.zsh

--- .config/zsh/plugins/homebrew.zsh
+++ .config/zsh/plugins/homebrew.zsh
@@ -19,7 +19,15 @@ if [ "$OS_TYPE" = "Darwin" ]; then
         FPATH="$(brew --prefix)/share/zsh/site-functions:$FPATH"
     fi
 
+    # 手动设置
+    export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+  
+    # 设置 homebrew/core 和 homebrew/cask 镜像。
     brew tap --custom-remote --force-auto-update homebrew/core https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
     brew tap --custom-remote --force-auto-update homebrew/cask https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
+  
+    # 除 homebrew/core 和 homebrew/cask 仓库外的 tap 仓库仍然需要设置镜像
+    brew tap --custom-remote --force-auto-update homebrew/command-not-found https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-command-not-found.git
+    brew update
 
 fi

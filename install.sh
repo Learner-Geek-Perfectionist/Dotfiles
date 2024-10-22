c60c2779c558a66465d7a3b910d1c15a76a15563
信萌新 <1273730703@qq.com>
2024-10-23 05:37:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -570,7 +570,10 @@ elif [[ $OS_TYPE == "Linux" ]]; then
   if [[ $os_type == "ubuntu" ]]; then
     sudo sed -i.bak -r 's|^#?(deb\|deb-src) http://archive.ubuntu.com/ubuntu/|\1 https://mirrors.ustc.edu.cn/ubuntu/|' /etc/apt/sources.list
     sudo apt update && sudo apt upgrade -y
-    sudo apt install -y openssh-server net-tools git unzip fzf ninja-build neovim ruby-full cmake nodejs iputils-ping procps htop traceroute tree coreutils zsh fontconfig python3 iproute2 kitty wget2 pkg-config graphviz kotlin golang zip software-properties-common
+    sudo apt install -y openssh-server net-tools git unzip ninja-build neovim ruby-full cmake nodejs iputils-ping procps htop traceroute tree coreutils zsh fontconfig python3 iproute2 kitty wget2 pkg-config graphviz kotlin golang zip software-properties-common 
+
+    # 手动安装 fzf
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     
     # 安装 Kotlin/Native
     install_kotlin_native "linux"

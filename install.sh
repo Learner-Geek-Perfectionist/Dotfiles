f13733ebbde356491cb5abc272c88cf639e50011
信萌新 <1273730703@qq.com>
2024-10-23 16:38:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -405,7 +405,8 @@ if [[ $OS_TYPE == "Darwin" ]]; then
     # 刷新 brew 配置，启用 brew 环境变量
     source ${HOME}/.zprofile
   fi
-  
+
+  [[ -f "./Homebrew.sh" ]] && rm "./Homebrew.sh" && echo "文件已被删除。" || echo "文件不存在。"
    
   print_centered_message "为了能顺利安装 Homebrew 的 cask 包，请打开代理软件，否则下载速度很慢（推荐选择香港 🇭🇰  或者 新加坡 🇸🇬  节点，如果速度还是太慢，可以通过客户端查看代理情况）" "true" "false"
   print_centered_message "如果下载进度条卡住，在代理客户端中，多次切换「全局模式」或者「规则模式」，并且打开 TUN 选项。" "false" "true"

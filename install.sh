110ba2eeba1ecd8848f41cf435855de5c4cfd2c3
信萌新 <1273730703@qq.com>
2024-11-08 02:42:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -192,8 +192,8 @@ install_and_configure_docker() {
     # 检查 Docker 是否已经安装
     echo "检查 Docker 命令..."
     docker_cmd=$(command -v docker)
-    if [ -z "$docker_cmd" ]; then
-        echo "Docker 未安装，开始安装过程..."
+    if [ -z "$docker_cmd" ] || [[ "$docker_cmd" == "/mnt/c/"* ]]; then
+        echo "Docker 未安装或未正确配置在WSL2中，开始安装过程..."
 
         # 1. 获取安装脚本
         curl -fsSL https://get.docker.com -o get-docker.sh

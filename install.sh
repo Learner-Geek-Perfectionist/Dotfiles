36d5d67ac029a38339952bf90fdc19b3bda46db5
信萌新 <1273730703@qq.com>
2024-11-09 19:43:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -740,7 +740,7 @@ elif [[ $OS_TYPE == "Linux" ]]; then
       /etc/yum.repos.d/fedora-updates.repo
 
     # 安装必要的工具 🔧
-    sudo dnf -y update && sudo dnf install -y glibc glibc-common openssh-server iproute net-tools fd-find git unzip zip ripgrep fastfetch fzf ninja-build neovim ruby kitty cmake nodejs iputils procps-ng htop traceroute tree coreutils zsh fontconfig python3 wget pkgconf-pkg-config graphviz wireshark tcpdump java-latest-openjdk golang rust glibc-locale-source glibc-langpack-zh openssl && sudo dnf install -y --setopt=tsflags= coreutils coreutils-common man-pages man-db  jq && sudo dnf group install -y --setopt=strict=0 "c-development" 
+    sudo dnf -y update && sudo dnf install -y glibc glibc-common openssh-server iproute net-tools fd-find git unzip zip ripgrep fastfetch fzf ninja-build neovim ruby kitty cmake nodejs iputils procps-ng htop traceroute tree coreutils zsh fontconfig python3 wget pkgconf-pkg-config graphviz wireshark tcpdump java-latest-openjdk golang rust glibc-locale-source glibc-langpack-zh jq openssl && sudo dnf install -y --setopt=tsflags= coreutils coreutils-common man-pages man-db && sudo dnf group install -y --setopt=strict=0 "c-development" 
 
     # 设置 wireshark 权限 
     # 1. 将 dumpcap 设置为允许 wireshark 组的成员执行：

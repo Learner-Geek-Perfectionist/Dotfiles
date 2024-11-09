889e6715827fcf0d1841c582f493f47cbe584f05
ouyangzhaoxin <1273730703@qq.com>
2024-11-09 13:23:02 +0800

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# bootstrap.sh
+
+REMOTE_SCRIPT="https://raw.githubusercontent.com/Learner-Geek-Perfectionist/Dotfiles/refs/heads/master/install.sh"
+REMOTE_VERSION=$(curl -fsSL "${REMOTE_SCRIPT}?$(date +%s)" | grep 'Version' | cut -d' ' -f2)
+
+if [ "$LOCAL_VERSION" != "$REMOTE_VERSION" ]; then
+    curl -fsSL "${REMOTE_SCRIPT}?$(date +%s)" | /bin/bash
+fi

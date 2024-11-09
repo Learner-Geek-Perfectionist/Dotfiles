9da58f0a711679b020e53a55a43053a50848493e
信萌新 <1273730703@qq.com>
2024-11-09 14:22:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,10 +54,12 @@ print_centered_message() {
 LATEST_VERSION=""
 
 get_latest_version() {
+    
     # 使用 curl 获取 GitHub releases 最新的重定向地址，并且 grep 最新的版本号
     LATEST_VERSION=$(curl -s -L -I https://github.com/JetBrains/kotlin/releases/latest | grep -i location | sed -E 's/.*tag\/(v[0-9\.]+).*/\1/')
     # 输出最新的版本号，添加颜色
-    print_centered_message "${CYAN}The Latest Version of Kotlin/Native is $LATEST_VERSION${NC}" "true" "false"
+    print_centered_message "正在下载Kotlin/Native...... " "true" "false"
+    echo "${CYAN}The Latest Version of Kotlin/Native is $LATEST_VERSION${NC}" 
 }
 
 

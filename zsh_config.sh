e72358b900ef984b7fe518a0a040a0660df49ead
ouyangzhaoxin <1273730703@qq.com>
2024-11-18 04:12:30 +0800

    update

--- zsh_config.sh
+++ zsh_config.sh
@@ -3,12 +3,6 @@
 # 设置脚本在遇到错误时退出
 set -e
 
-# 定义颜色
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
-
 echo -e "${GREEN}🚀 Starting script...${NC}"
 
 rm -rf /tmp/Dotfiles
--- zsh_install.sh
+++ zsh_install.sh
@@ -37,19 +37,17 @@ dest_Fonts="Fonts-master"
 dest_Dotfiles="Dotfiles-master"
 
 # 对 Fonts 的处理：
-# 如果安装标志（$install_flag）为真，并且ZIP文件不存在，则下载并解压ZIP文件；如果ZIP文件已经存在，则检查目录是否存在，不存在则解压，存在则跳过解压。
-
 if [[ $install_flag == "true" ]]; then
     if [ ! -f "$zip_Fonts_file" ]; then
-        print_centered_message "Fonts ZIP 文件不存在，开始下载..."
+        print_centered_message "${YELLOW}Fonts ZIP 文件不存在，开始下载...${NC}"
         download_and_extract "$zip_Fonts_file" "$dest_Fonts" "$Fonts_REPO_URL"
     else
-        print_centered_message "Fonts ZIP 文件已存在，不需要下载。"
+        print_centered_message "${YELLOW}Fonts ZIP 文件已存在，不需要下载。${NC}"
         if [ ! -d "$dest_Fonts" ]; then
-            print_centered_message "开始解压已存在的 Fonts ZIP 文件..."
+            print_centered_message "${GREEN}开始解压已存在的 Fonts ZIP 文件...${NC}"
             unzip -o "$zip_Fonts_file" -d "$dest_Fonts"
         else
-            print_centered_message "Fonts 目录已存在，跳过解压。"
+            print_centered_message "${GREEN}Fonts 目录已存在，跳过解压。${NC}"
         fi
     fi
 fi
@@ -58,7 +56,7 @@ fi
 download_and_extract "$zip_Dotfiles_file" "$dest_Dotfiles" "$Dotfiles_REPO_URL"
 
 # 打印提示消息
-print_centered_message "Dotfile 完成下载和解压"
+print_centered_message "${GREEN}Dotfile 完成下载和解压${NC}"
 
 # 定义字体的源目录
 font_source="./${dest_Fonts}/fonts"
@@ -73,7 +71,7 @@ fi
 install_fonts
 
 # 打印提示消息
-print_centered_message "接下来配置 zsh......"
+print_centered_message "${YELLOW}接下来配置 zsh......${NC}"
 
 # 定义 zsh 的配置文件目录
 destination="$HOME"
@@ -82,9 +80,9 @@ destination="$HOME"
 source ./zsh_config.sh
 
 # 打印提示消息
-print_centered_message "zsh 配置文件已配置到 Home 目录"
+print_centered_message "${GREEN}zsh 配置文件已配置到 Home 目录${NC}"
 
-print_centered_message "进入 zsh，准备下载 zsh 插件......"
+print_centered_message "${RED}进入 zsh，准备下载 zsh 插件......${NC}"
 
 # 修改默认的登录 shell 为 zsh
 [[ $SHELL != */zsh ]] && echo "修改默认的 shell 为 zsh " && chsh -s $(which zsh)

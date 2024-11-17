b9a3bdba01b0078377c0d375d43d9db96aff1af8
ouyangzhaoxin <1273730703@qq.com>
2024-11-17 22:13:32 +0800

    update

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@
 set -e
 
 [ -d "Dotfiles" ] && rm -rf Dotfiles
+
+# 安装 git
+if grep -q 'ID=ubuntu' /etc/os-release; then
+    sudo apt install -y git
+elif grep -q 'ID=fedora' /etc/os-release; then
+    sudo dnf install -y git
+fi
+
+
 echo "Cloning Dotfiles repository..."
 GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone --depth=1 git@github.com:Learner-Geek-Perfectionist/Dotfiles.git && cd Dotfiles && echo "Changed directory to Dotfiles."
 

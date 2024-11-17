6cd44ca2627dce4d8092b5ceaf54e8bcb3162c16
信萌新 <1273730703@qq.com>
2024-11-18 00:55:35 +0800

    Update auxiliary_function.sh

--- auxiliary_function.sh
+++ auxiliary_function.sh
@@ -54,16 +54,17 @@ download_and_extract_kotlin() {
     echo -e "${YELLOW}Downloading ${BLUE}$FILE_NAME${YELLOW} from ${MAGENTA}$URL${YELLOW}...${NC}"
     # 使用 curl 下载文件，检查 URL 的有效性
     curl -L -f -s -S "$URL" -o "/tmp/$FILE_NAME" || {
-        echo -e "${RED}Failed to download $FILE_NAME. Please check your internet connection and URL.${NC}"
+        echo -e "${RED}Failed to download $FILE_NAME.\nPlease check your internet connection and URL.${NC}"
+    
         return 0
     }
 
     echo -e "${YELLOW}Installing ${GREEN}$FILE_NAME${YELLOW} to ${BLUE}$TARGET_DIR${YELLOW}...${NC}"
     sudo mkdir -p $TARGET_DIR
     if [[ $FILE_NAME == *.tar.gz ]]; then
-        tar -xzf "/tmp/$FILE_NAME" -C $TARGET_DIR --strip-components=1
+        sudo tar -xzf "/tmp/$FILE_NAME" -C $TARGET_DIR --strip-components=1
     elif [[ $FILE_NAME == *.zip ]]; then
-        unzip "/tmp/$FILE_NAME" -d $TARGET_DIR
+        sudo unzip "/tmp/$FILE_NAME" -d $TARGET_DIR
     fi
 
     if [ $? -ne 0 ]; then

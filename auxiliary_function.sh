bd4eb3812b7f229aecb1256373720ba114613a3a
ouyangzhaoxin <1273730703@qq.com>
2024-11-17 23:03:29 +0800

    update

--- auxiliary_function.sh
+++ auxiliary_function.sh
@@ -52,7 +52,9 @@ download_and_extract_kotlin() {
     FILE_NAME=$(basename $URL)
 
     echo -e "${YELLOW}Downloading ${BLUE}$FILE_NAME${YELLOW} from ${MAGENTA}$URL${YELLOW}...${NC}"
-    curl -L $URL -o "/tmp/$FILE_NAME"
+    # 使用 curl 下载文件，检查 URL 的有效性
+    curl -L -f -s -S "$URL" -o "/tmp/$FILE_NAME"
+    # 检查上一个命令的退出状态
     if [ $? -ne 0 ]; then
         echo -e "${RED}Failed to download $FILE_NAME. Please check your internet connection and URL.${NC}"
         return 0

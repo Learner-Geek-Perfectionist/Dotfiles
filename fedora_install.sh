b55a8a5a7f708c34965f1d59fa26189dc5f97874
信萌新 <1273730703@qq.com>
2024-11-18 03:30:13 +0800

    Update fedora_install.sh

--- fedora_install.sh
+++ fedora_install.sh
@@ -35,10 +35,11 @@ export LC_ALL=zh_CN.UTF-8
 
 sudo localedef -c -f UTF-8 -i zh_CN zh_CN.UTF-8
 
-setup_kotlin_environment
+setup_kotlin_environment 
 # 安装 Kotlin/Native 和 Kotlin
-download_and_extract_kotlin $KOTLIN_NATIVE_URL $INSTALL_DIR
-download_and_extract_kotlin $KOTLIN_COMPILER_URL $COMPILER_INSTALL_DIR
+download_and_extract_kotlin $KOTLIN_NATIVE_URL $INSTALL_DIR "Kotlin/Native"
+
+download_and_extract_kotlin $KOTLIN_COMPILER_URL $COMPILER_INSTALL_DIR "Kotlin-Complier"
 
 # 调用函数以安装和配置 Docker
 install_and_configure_docker

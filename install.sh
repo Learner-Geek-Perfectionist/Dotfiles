289632f6e20e18641f7e41c88ad1f28e78d43bc0
ouyangzhaoxin <1273730703@qq.com>
2024-11-17 15:30:43 +0800

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 # 一旦错误，就退出
 set -e
 
-git clone --depth=1 git@github.com:Learner-Geek-Perfectionist/Dotfiles.git
+[ -d "Dotfiles" ] && rm -rf Dotfiles;
+echo "Cloning Dotfiles repository...";
+git clone --depth=1 git@github.com:Learner-Geek-Perfectionist/Dotfiles.git && cd Dotfiles && echo "Changed directory to Dotfiles." &&
 
 cd Dotfiles
 

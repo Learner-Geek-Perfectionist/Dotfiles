d1203317deb50e80f929ff7644e64beff8b24364
ouyangzhaoxin <1273730703@qq.com>
2024-11-19 09:59:55 +0800

    update

--- macos_install.sh
+++ macos_install.sh
@@ -9,7 +9,7 @@ cd $HOME/Documents
 if ! xcode-select --print-path &> /dev/null; then
     print_centered_message "${RED}⚠️ Xcode 命令行工具未安装${NC}" "true" "false"
     xcode-select --install 2> /dev/null
-    print_centered_message "${RED}请手动点击屏幕中的弹窗，选择“安装”，安装完成之后再次运行脚本(提示命令通常在终端的背面)${NC}" "false" "false"
+    print_centered_message "${RED}请手动点击屏幕中的弹窗，选择"安装"，安装完成之后再次运行脚本(提示命令通常在终端的背面)${NC}" "false" "false"
     echo -e "${RED}脚本命令: ${NC}"
     print_centered_message "${RED}/bin/zsh -c \"$(curl -fsSL https://raw.githubusercontent.com/Learner-Geek-Perfectionist/Dotfiles/refs/heads/master/install.sh)\"${NC}" "false" "true"
     exit 1

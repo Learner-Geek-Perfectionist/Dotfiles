05704a29de1a4f3b8bf065fbc1cb5f48bddf9651
ouyangzhaoxin <1273730703@qq.com>
2024-11-19 10:54:47 +0800

    update

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ if [[ $(uname -s) == "Linux" ]]; then
 fi
 
 echo -e "${BLUE}Cloning Dotfiles repository...${NC}"
-git clone --depth=1 https://github.com/Learner-Geek-Perfectionist/Dotfiles.git /tmp/Dotfiles && cd /tmp/Dotfiles && echo -e "${GREEN}Changed directory to Dotfiles.${NC}"
+git clone --depth=1 https://github.com/Learner-Geek-Perfectionist/Dotfiles.git /tmp/Dotfiles && cd /tmp/Dotfiles && echo -e "${GREEN}Changed directory to $(pwd).${NC}"
+
 # 执行安装脚本
 source ./main.sh

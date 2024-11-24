45bd030dad1ae755a9c917bf4b140dc722da3a76
信萌新 <1273730703@qq.com>
2024-11-25 00:43:12 +0800

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -7,70 +7,25 @@ set -e
 sudo sed -i.bak -r 's|^#?(deb\|deb-src) http://archive.ubuntu.com/ubuntu/|\1 https://mirrors.ustc.edu.cn/ubuntu/|' /etc/apt/sources.list && sudo apt update 
 
 # =================================开始安装 wireshark=================================
-# if ! command -v wireshark >/dev/null 2>&1; then
-#     if  (sudo DEBIAN_FRONTEND=noninteractive apt-add-repository -y "ppa:wireshark-dev/stable" >/dev/null 2>&1 && sudo apt update >/dev/null 2>&1); then
-#         print_centered_message  "${GREEN}PPA支持您的Ubuntu版本 ✅。继续安装 ${RED}wireshark...${NC}"  "false" "true"
-#         sudo DEBIAN_FRONTEND=noninteractive apt install -y wireshark
-#         print_centered_message   "${GREEN}Wireshark 安装完成 ✅${NC}"  "false" "false"
-#     else
-#         print_centered_message  "${RED}❌ PPA不支持您的Ubuntu版本...${NC}" "false" "false"
-#         sudo add-apt-repository -r -y "ppa:wireshark-dev/stable" >/dev/null 2>&1  # 移除PPA
-#     fi
-
-# else
-#     print_centered_message   "${GREEN}Wireshark 已安装，跳过安装。${NC}" "false" "false"
-# fi
-
-sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:wireshark-dev/stable
-sudo DEBIAN_FRONTEND=noninteractive apt install -y wireshark
+if ! command -v wireshark >/dev/null 2>&1; then
+    print_centered_message   "${GREEN}开始安装 wireshark${NC}" "true" "false"
+    sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:wireshark-dev/stable
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y wireshark
+else
+    print_centered_message   "${GREEN}Wireshark 已安装，跳过安装。${NC}" "false" "false"
+fi
 # =================================结束安装 wireshark=================================
 
 
 # =================================开始安装 fastfetch=================================
 if ! command -v fastfetch > /dev/null 2>&1; then
-    FASTFETCH_LATEST_VERSION=$(curl -s -L -I https://github.com/fastfetch-cli/fastfetch/releases/latest | grep -i location | sed -E 's|.*tag/([0-9\.]+).*|\1|')
+    print_centered_message   "${GREEN}开始安装 fastfetch${NC}" "true" "false"
+    sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:zhangsongcui3371/fastfetch &&  sudo apt update
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y fastfetch
     
-    # 确定架构
-    ARCH=$(uname -m)
-    case "$ARCH" in
-        x86_64) FASTFETCH_ARCH="amd64" ;;
-        aarch64) FASTFETCH_ARCH="aarch64" ;;
-        *) echo -e "${RED}Unsupported architecture: $ARCH${NC}" && exit 1 ;;
-    esac
-    
-    
-    # 确定系统类型
-    case "$(uname -s)" in
-        Linux)
-            SYSTEM_TYPE="linux"
-            ;;
-        *)
-            echo -e "${RED}Unsupported system type: $(uname -s)${NC}"
-            exit 1
-            ;;
-    esac
-
-    URL="https://github.com/fastfetch-cli/fastfetch/releases/download/${FASTFETCH_LATEST_VERSION}/fastfetch-${SYSTEM_TYPE}-${FASTFETCH_ARCH}.deb"
-    FILE_NAME=$(basename $URL)
-    
-    print_centered_message "${LIGHT_BLUE}正在下载 ${FILE_NAME}...... ${NC}" "true" "false"
-    echo -e "${CYAN}The Latest Version is ${RED}${FASTFETCH_LATEST_VERSION}${NC}"
-    echo -e "${YELLOW}Downloading ${BLUE}${FILE_NAME}${YELLOW} from ${MAGENTA}${URL}${NC}"
-    
-    
-    # 使用 curl 下载文件，检查 URL 的有效性
-    curl -L -f -s -S "${URL}" -o "/tmp/$FILE_NAME" || {
-        print_centered_message  "${RED}❌ Failed to download ${FILE_NAME}.Please check your internet connection and URL.${NC}" "false" "false"
-        return 0
-    }
-    
-    sudo apt install -y /tmp/${FILE_NAME}
-    
-    print_centered_message "${GREEN} ${FILE_NAME} 安装完成 ✅${NC}" "false" "false"
 else
      print_centered_message   "${GREEN} fastfetch 已安装，跳过安装。${NC}" "true" "false"
 fi
-
 # =================================结束安装 fastfetch=================================
 
 

d363cf22f13334ca26eac363d83822544c041153
信萌新 <1273730703@qq.com>
2024-11-25 01:55:11 +0800

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -19,10 +19,45 @@ fi
 
 # =================================开始安装 fastfetch=================================
 if ! command -v fastfetch > /dev/null 2>&1; then
-    print_centered_message   "${GREEN}开始安装 fastfetch${NC}" "true" "false"
-    sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:zhangsongcui3371/fastfetch &&  sudo apt update
-    sudo DEBIAN_FRONTEND=noninteractive apt install -y fastfetch
+    FASTFETCH_LATEST_VERSION=$(curl -s -L -I https://github.com/fastfetch-cli/fastfetch/releases/latest | grep -i location | sed -E 's|.*tag/([0-9\.]+).*|\1|')
     
+    # 确定架构
+    ARCH=$(uname -m)
+    case "$ARCH" in
+        x86_64) FASTFETCH_ARCH="amd64" ;;
+        aarch64) FASTFETCH_ARCH="aarch64" ;;
+        *) echo -e "${RED}Unsupported architecture: $ARCH${NC}" && exit 1 ;;
+    esac
+    
+    
+    # 确定系统类型
+    case "$(uname -s)" in
+        Linux)
+            SYSTEM_TYPE="linux"
+            ;;
+        *)
+            echo -e "${RED}Unsupported system type: $(uname -s)${NC}"
+            exit 1
+            ;;
+    esac
+
+    URL="https://github.com/fastfetch-cli/fastfetch/releases/download/${FASTFETCH_LATEST_VERSION}/fastfetch-${SYSTEM_TYPE}-${FASTFETCH_ARCH}.deb"
+    FILE_NAME=$(basename $URL)
+    
+    print_centered_message "${GREEN}正在下载 ${FILE_NAME}...... ${NC}" "true" "false"
+    echo -e "${CYAN}The Latest Version is ${RED}${FASTFETCH_LATEST_VERSION}${NC}"
+    echo -e "${YELLOW}Downloading ${BLUE}${FILE_NAME}${YELLOW} from ${MAGENTA}${URL}${NC}"
+    
+    
+    # 使用 curl 下载文件，检查 URL 的有效性
+    curl -L -f -s -S "${URL}" -o "/tmp/$FILE_NAME" || {
+        print_centered_message  "${RED}❌ Failed to download ${FILE_NAME}.Please check your internet connection and URL.${NC}" "false" "false"
+        return 0
+    }
+    
+    sudo apt install -y /tmp/${FILE_NAME}
+    
+    print_centered_message "${GREEN} ${FILE_NAME} 安装完成 ✅${NC}" "false" "false"
 else
      print_centered_message   "${GREEN} fastfetch 已安装，跳过安装。${NC}" "true" "false"
 fi
@@ -47,8 +82,8 @@ if ! command -v kitty > /dev/null 2>&1; then
         sed -i "s|Exec=kitty|Exec=$HOME/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
         # Allow-launching of the shortcut:
         DESKTOP_PATH=$(xdg-user-dir DESKTOP)
-        gio set ${DESKTOP_PATH}/kitty*.desktop metadata::trusted true
-        chmod a+x ~/Desktop/kitty*.desktop
+        gio set /home/oyzx/.local/kitty.app/share/applications/kitty-open.desktop metadata::trusted true && gio set /home/oyzx/.local/kitty.app/share/applications/kitty.desktop metadata::trusted true && gio set /home/oyzx/.local/share/applications/kitty-open.desktop metadata::trusted true && gio set /home/oyzx/.local/share/applications/kitty.desktop metadata::trusted true
+        chmod a+x /home/oyzx/.local/kitty.app/share/applications/kitty-open.desktop /home/oyzx/.local/kitty.app/share/applications/kitty.desktop /home/oyzx/.local/share/applications/kitty-open.desktop /home/oyzx/.local/share/applications/kitty.desktop
     fi
 else
     print_centered_message   "${GREEN} kitty 已安装，跳过安装。${NC}" "true" "false"

dc13219cfbbcc8c458f30ac9bab54955091ef0a5
ouyangzhaoxin <1273730703@qq.com>
2024-12-08 02:08:12 +0800

    update

---
+++ .hammerspoon/hyper-apps-defaults.lua
@@ -0,0 +1,12 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'w', 'WeChat' },            -- "W" for "WeChat"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 'g', 'ChatGPT' },           -- "G" ChatGPT"
+  { 'r', 'Safari' },            -- "R" for "Safari"
+  { 't', 'kitty' },             -- "T" for "kitty"
+}

be6430526e0bdcc9a776c5da49c1dff77ba2e01a
ouyangzhaoxin <1273730703@qq.com>
2024-12-08 23:18:52 +0800

    update

--- .hammerspoon/function.lua
+++ .hammerspoon/function.lua
@@ -2,11 +2,6 @@ local mod = {}  -- 创建一个新的表来作为模块
 -- 定义一个函数，输入参数为 bundleID
 function mod.launchAppByBundleID(bundleID)
     -- 使用 hs.application.launchOrFocusByBundleID 函数根据 bundleID 打开应用
-    local success = hs.application.launchOrFocusByBundleID(bundleID)
-    if success then
-        print("Application launched successfully")
-    else
-        print("Failed to launch application")
-    end
+    hs.application.launchOrFocusByBundleID(bundleID)
 end
 return mod  -- 返回这个表作为模块的输出
--- .hammerspoon/key-binding.lua
+++ .hammerspoon/key-binding.lua
@@ -3,7 +3,7 @@ local status, hyperModeAppMappings = pcall(require, 'key-mapping')
 -- 检查模块是否成功加载
 if not status then
     hs.logger.new('hyper', 'error'):e('Failed to load key-mapping module')
-    return  -- 加载失败时提前退出
+    return -- 加载失败时提前退出
 end
 
 -- 加载 function 模块
@@ -12,7 +12,7 @@ local mod = require("function")
 -- 遍历映射并绑定快捷键
 for _, appMapping in ipairs(hyperModeAppMappings) do
     local key, appID = appMapping[1], appMapping[2]
-    hs.hotkey.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, key, function()
+    hs.hotkey.bind({ 'ctrl', 'alt', 'cmd' }, key, function()
         mod.launchAppByBundleID(appID)
     end)
 end

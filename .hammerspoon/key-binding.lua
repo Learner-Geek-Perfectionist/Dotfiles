a88383c167f6642a69996e23b9fff20781a9abaf
ouyangzhaoxin <1273730703@qq.com>
2024-12-08 23:33:15 +0800

    update

--- .hammerspoon/key-binding.lua
+++ .hammerspoon/key-binding.lua
@@ -12,7 +12,7 @@ local mod = require("function")
 -- 遍历映射并绑定快捷键
 for _, appMapping in ipairs(hyperModeAppMappings) do
     local key, appID = appMapping[1], appMapping[2]
-    hs.hotkey.bind({ 'ctrl', 'alt', 'cmd' }, key, function()
+    hs.hotkey.bind({ 'ctrl', 'alt', 'cmd', 'shift' }, key, function()
         mod.launchAppByBundleID(appID)
     end)
 end

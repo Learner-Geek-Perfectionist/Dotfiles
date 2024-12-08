e9b3dd71e97f2e91586420021b634a26ebdd9691
ouyangzhaoxin <1273730703@qq.com>
2024-12-08 23:34:38 +0800

    update

--- .hammerspoon/function.lua
+++ .hammerspoon/function.lua
@@ -1,7 +1,19 @@
 local mod = {}  -- 创建一个新的表来作为模块
+
 -- 定义一个函数，输入参数为 bundleID
-function mod.launchAppByBundleID(bundleID)
-    -- 使用 hs.application.launchOrFocusByBundleID 函数根据 bundleID 打开应用
-    hs.application.launchOrFocusByBundleID(bundleID)
+function mod.launchOrToggleAppByBundleID(bundleID)
+    local app = hs.application.get(bundleID)  -- 获取应用对象
+
+    -- 检查应用是否存在且有打开的窗口
+    if app and #app:allWindows() > 0 then
+        if app:isFrontmost() then
+            app:hide()  -- 如果应用已经是最前端，则隐藏它
+        else
+            app:activate()  -- 否则，激活应用
+        end
+    else
+        hs.application.launchOrFocusByBundleID(bundleID)  -- 如果应用没有运行或没有窗口，启动它
+    end
 end
+
 return mod  -- 返回这个表作为模块的输出

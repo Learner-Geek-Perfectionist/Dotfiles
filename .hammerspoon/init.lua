8e2b508e7145fc95ccc1fc6360824db31aa61ae6
ouyangzhaoxin <1273730703@qq.com>
2024-12-16 00:33:42 +0800

    update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,9 +1,10 @@
 ---@diagnostic disable: lowercase-global
 
--- 加载 hyper.lua
+
 require('config.keyConfig')
 require('config.KeyBinds')
 require('modules.reload')
+require('modules.systemInfo')
 require('modules.AppToggler')
 require('modules.windowManagement')
 require('modules.inputMethod')
@@ -11,5 +12,6 @@ require('modules.inputMethod')
 --  启动 inputMethod 模块
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
+
 
 hs.notify.new({ title = 'Hammerspoon', informativeText = 'Ready to rock 🤘' }):send()

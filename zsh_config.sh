47a0863e3c297fa48a1f2bda1082f2ed7bd7be65
ouyangzhaoxin <1273730703@qq.com>
2024-12-16 11:36:38 +0800

    update

--- zsh_config.sh
+++ zsh_config.sh
@@ -42,12 +42,12 @@ for config in "${configs[@]}"; do
     echo -e "${RED}🗑️ Removing old ${config}...${NC}"
     sudo rm -rf "${HOME}/$config"
   fi
-  echo -e "${PURPLE}📋 Copying new ${config} to ${HOME}...${NC}"
-  sudo cp -r "${TMP_DIR}/${config}" "${HOME}/${config}"
+  echo -e "${PURPLE}📋 Moving new ${config} to ${HOME}...${NC}"
+  sudo mv -r "${TMP_DIR}/${config}" "${HOME}/${config}"
 done
 
 # 在文件中添加以下代码
-[[ "$(uname)" == "Darwin" ]] && sudo cp -r "$TMP_DIR/sh-script/" "$HOME/sh-script/"
+[[ "$(uname)" == "Darwin" ]] && sudo mv -r "$TMP_DIR/sh-script/" "$HOME/sh-script/"
 
 # 添加 .hammerspoon 文件夹
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -56,7 +56,7 @@ if [[ "$(uname)" == "Darwin" ]]; then
     sudo rm -rf "${HOME}/.hammerspoon"
   fi
   echo -e "${PURPLE}📋 Copying new .hammerspoon to "${HOME}/.hammerspoon"...${NC}"
-  sudo cp -r "${TMP_DIR}/.hammerspoon" "${HOME}/.hammerspoon" && sudo chown -R $USER:$(id -gn) "${HOME}/.hammerspoon"
+  sudo mv -r "${TMP_DIR}/.hammerspoon" "${HOME}/.hammerspoon" && sudo chown -R $USER:$(id -gn) "${HOME}/.hammerspoon"
 fi
 
 # 添加 Karabiner 配置文件：capslock2hyper.json
@@ -66,7 +66,7 @@ if [[ "$(uname)" == "Darwin" ]]; then
     sudo rm -rf "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json"
   fi
   echo -e "${PURPLE}📋 Copying new capslock2hyper.json to ${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json...${NC}"
-  sudo cp -r "${TMP_DIR}/capslock2hyper.json" "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json" && sudo chown -R $USER:$(id -gn) "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json"
+  sudo mv -r "${TMP_DIR}/capslock2hyper.json" "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json" && sudo chown -R $USER:$(id -gn) "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json"
 fi
 
 echo -e "${GREEN}🧹 Old configuration files removed and new ones copied.${NC}"

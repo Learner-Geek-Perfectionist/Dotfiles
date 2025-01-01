602ac7d21c5231a145de3696869a4e892db2edc2
ouyangzhaoxin <1273730703@qq.com>
2025-01-01 13:37:37 +0800

    update

--- zsh_config.sh
+++ zsh_config.sh
@@ -55,37 +55,20 @@ sudo rm -rf /tmp/Fonts/
 echo -e "${GREEN}🚀 Starting script...${NC}"
 cd $HOME
 
-
-# 浅克隆仓库到临时目录
-echo -e "${YELLOW}📥 Cloning repository into $TMP_DIR...${NC}"
-git clone --depth 1 https://github.com/Learner-Geek-Perfectionist/Dotfiles "$TMP_DIR" || {
-  echo "Failed to clone repository"
-  exit 1
-}
-
-# Ensure XDG base directories exist
-mkdir -p "$HOME/.config/zsh/plugins" "${HOME}/.config/kitty" "$HOME/.cache/zsh" "${HOME}/.local/share/zinit" "$HOME/.local/state"
-
-
-if ! command -v zsh > /dev/null 2>&1 ; then
-  echo -e "${RED}zsh 未安装${NC}"
-  exit 1
-fi
-
 # 安装依赖工具 eza、fzf、kitty
 
 if [[ $(uname -s) == "Darwin" ]]; then
-  if ! command -v fzf > /dev/null 2>&1; then
-      brew install -y fzf
-  fi
+    if ! command -v fzf >/dev/null 2>&1; then
+        brew install -y fzf
+    fi
 
-  if ! command -v eza > /dev/null 2>&1; then
-      brew install -y eza
-  fi
+    if ! command -v eza >/dev/null 2>&1; then
+        brew install -y eza
+    fi
 
-  if ! command -v kitty > /dev/null 2>&1; then
-      brew install -y kitty
-  fi
+    if ! command -v kitty >/dev/null 2>&1; then
+        brew install -y kitty
+    fi
 
 elif [[ $(uname -s) == "Linux" ]]; then
 
@@ -95,11 +78,21 @@ elif [[ $(uname -s) == "Linux" ]]; then
     # 根据操作系统安装......
     if [[ $os_type == "ubuntu" ]]; then
 
-    # 解压的依赖工具 xz
-    sudo apt install -y xz-utils
-    # =================================开始安装 kitty=================================
-        if ! command -v kitty > /dev/null 2>&1; then
-            print_centered_message  "${GREEN}开始安装 kitty... ${NC}" "true" "false"
+        # 解压的依赖工具 xz
+        sudo apt install -y xz-utils
+        if ! command -v zsh >/dev/null 2>&1; then
+            sudo apt install -y zsh
+        fi
+
+        if ! command -v git >/dev/null 2>&1; then
+            sudo apt install -y git
+        fi
+        if ! command -v curl >/dev/null 2>&1; then
+            sudo apt install -y curl
+        fi
+        # =================================开始安装 kitty=================================
+        if ! command -v kitty >/dev/null 2>&1; then
+            print_centered_message "${GREEN}开始安装 kitty... ${NC}" "true" "false"
             curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
 
             # 定义基础 URL
@@ -137,7 +130,7 @@ elif [[ $(uname -s) == "Linux" ]]; then
 
             # 检查是否在 WSL2 中运行或在自动化脚本环境中
             if grep -qi microsoft /proc/version || [[ "$AUTO_RUN" == "true" ]]; then
-                print_centered_message  "${RED}在 WSL2 中或者 Dockerfile 中不需要安装 kitty 桌面图标${NC}" "false" "false"
+                print_centered_message "${RED}在 WSL2 中或者 Dockerfile 中不需要安装 kitty 桌面图标${NC}" "false" "false"
             else
                 mkdir -p /usr/local/bin/ ~/.local/share/applications/
                 sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/
@@ -150,50 +143,58 @@ elif [[ $(uname -s) == "Linux" ]]; then
                 sudo chmod a+x $HOME/.local/kitty.app/share/applications/kitty-open.desktop $HOME/.local/kitty.app/share/applications/kitty.desktop $HOME/.local/share/applications/kitty-open.desktop $HOME/.local/share/applications/kitty.desktop
             fi
         else
-            print_centered_message   "${GREEN} kitty 已安装，跳过安装。${NC}" "true" "false"
+            print_centered_message "${GREEN} kitty 已安装，跳过安装。${NC}" "true" "false"
         fi
 
-    # =================================结束安装 kitty=================================
+        # =================================结束安装 kitty=================================
 
-    # =================================开始安装 fzf=================================
-        if command -v fzf > /dev/null 2>&1; then
-            print_centered_message  "${GREEN}fzf 已安装，跳过安装。${NC}"  "true" "false"
+        # =================================开始安装 fzf=================================
+        if command -v fzf >/dev/null 2>&1; then
+            print_centered_message "${GREEN}fzf 已安装，跳过安装。${NC}" "true" "false"
         else
-            print_centered_message  "${GREEN}开始安装 fzf... ${NC}" "true" "false"
+            print_centered_message "${GREEN}开始安装 fzf... ${NC}" "true" "false"
             [[ -d "$HOME/.fzf" ]] && rm -rf "$HOME/.fzf"
 
             git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
             yes | $HOME/.fzf/install --no-update-rc
             print_centered_message "${GREEN} fzf 安装完成 ✅${NC}" "false" "false"
         fi
-    # =================================结束安装 fzf=================================
+        # =================================结束安装 fzf=================================
 
-
-    # =================================开始安装 eza=================================
-    if command -v eza > /dev/null 2>&1; then
-        print_centered_message  "${GREEN}eza 已安装，跳过安装。${NC}"  "true" "true"
-    else
-        print_centered_message  "${GREEN}开始安装 eza... ${NC}" "true" "false"
-        # 安装 eza, 在 oracular (24.10)  之后的 Ubuntu 发行版才有
-        ! command -v cargo > /dev/null 2>&1 && sudo apt install -y cargo
-        cargo install eza
-        print_centered_message "${GREEN} eza 安装完成 ✅${NC}" "false" "false"
-    fi
+        # =================================开始安装 eza=================================
+        if command -v eza >/dev/null 2>&1; then
+            print_centered_message "${GREEN}eza 已安装，跳过安装。${NC}" "true" "true"
+        else
+            print_centered_message "${GREEN}开始安装 eza... ${NC}" "true" "false"
+            # 安装 eza, 在 oracular (24.10)  之后的 Ubuntu 发行版才有
+            ! command -v cargo >/dev/null 2>&1 && sudo apt install -y cargo
+            cargo install eza
+            print_centered_message "${GREEN} eza 安装完成 ✅${NC}" "false" "false"
+        fi
     # =================================结束安装 eza=================================
 
-
     elif [[ $os_type == "fedora" ]]; then
-         if ! command -v fzf > /dev/null 2>&1; then
-              sudo dnf install -y fzf
-         fi
+        if ! command -v fzf >/dev/null 2>&1; then
+            sudo dnf install -y fzf
+        fi
 
-         if ! command -v eza > /dev/null 2>&1; then
-              sudo dnf install -y eza
-         fi
+        if ! command -v eza >/dev/null 2>&1; then
+            sudo dnf install -y eza
+        fi
 
-         if ! command -v kitty > /dev/null 2>&1; then
-              sudo dnf install -y kitty
-         fi
+        if ! command -v kitty >/dev/null 2>&1; then
+            sudo dnf install -y kitty
+        fi
+
+        if ! command -v zsh >/dev/null 2>&1; then
+            sudo dnf install -y zsh
+        fi
+        if ! command -v git >/dev/null 2>&1; then
+            sudo dnf install -y git
+        fi
+        if ! command -v curl >/dev/null 2>&1; then
+            sudo dnf install -y curl
+        fi
 
     else
         print_centered_message "${RED}不支持的发行版，目前只支持 fedora、ubuntu${NC}"
@@ -208,6 +209,15 @@ else
     echo -e "${MAGENTA}未知的操作系统类型${NC}"
 fi
 
+# 浅克隆仓库到临时目录
+echo -e "${YELLOW}📥 Cloning repository into $TMP_DIR...${NC}"
+git clone --depth 1 https://github.com/Learner-Geek-Perfectionist/Dotfiles "$TMP_DIR" || {
+    echo "Failed to clone repository"
+    exit 1
+}
+
+# Ensure XDG base directories exist
+mkdir -p "$HOME/.config/zsh/plugins" "${HOME}/.config/kitty" "$HOME/.cache/zsh" "${HOME}/.local/share/zinit" "$HOME/.local/state"
 
 # 定义配置列表
 configs=(".zshenv" ".zprofile" ".zshrc" ".config/kitty" ".config/zsh")
@@ -215,34 +225,34 @@ configs=(".zshenv" ".zprofile" ".zshrc" ".config/kitty" ".config/zsh")
 # 删除旧配置和复制新配置
 echo -e "${YELLOW}🔍 Checking and removing old configuration files if they exist...${NC}"
 for config in "${configs[@]}"; do
-  if [[ -f "${HOME}/${config}" ]] || [[ -d "${HOME}/${config}" ]]; then
-    echo -e "${RED}🗑️ Removing old ${config}...${NC}"
-    sudo rm -rf "${HOME}/$config"
-  fi
-  echo -e "${PURPLE}📋 Moving new ${config} to ${HOME}...${NC}"
+    if [[ -f "${HOME}/${config}" ]] || [[ -d "${HOME}/${config}" ]]; then
+        echo -e "${RED}🗑️ Removing old ${config}...${NC}"
+        sudo rm -rf "${HOME}/$config"
+    fi
+    echo -e "${PURPLE}📋 Moving new ${config} to ${HOME}...${NC}"
     cp -r "${TMP_DIR}/${config}" "${HOME}/${config}"
 done
 
 # 在文件中添加以下代码
-  [[ "$(uname)" == "Darwin" ]] && cp -r "$TMP_DIR/sh-script/" "$HOME/sh-script/"
+[[ "$(uname)" == "Darwin" ]] && cp -r "$TMP_DIR/sh-script/" "$HOME/sh-script/"
 
 # 添加 .hammerspoon 文件夹
 if [[ "$(uname)" == "Darwin" ]]; then
-  if [[ -d "${HOME}/.hammerspoon" ]]; then
-    echo -e "${RED}🗑️ Removing old .hammerspoon...${NC}"
-    sudo rm -rf "${HOME}/.hammerspoon"
-  fi
-  echo -e "${PURPLE}📋 Copying new .hammerspoon to "${HOME}/.hammerspoon"...${NC}"
+    if [[ -d "${HOME}/.hammerspoon" ]]; then
+        echo -e "${RED}🗑️ Removing old .hammerspoon...${NC}"
+        sudo rm -rf "${HOME}/.hammerspoon"
+    fi
+    echo -e "${PURPLE}📋 Copying new .hammerspoon to "${HOME}/.hammerspoon"...${NC}"
     cp -r "${TMP_DIR}/.hammerspoon" "${HOME}/.hammerspoon"
 fi
 
 # 添加 Karabiner 配置文件：capslock2hyper.json
 if [[ "$(uname)" == "Darwin" ]]; then
-  if [[ -f "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json" ]]; then
-    echo -e "${RED}🗑️ Removing old capslock2hyper.json...${NC}"
-    sudo rm -rf "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json"
-  fi
-  echo -e "${PURPLE}📋 Copying new capslock2hyper.json to ${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json...${NC}"
+    if [[ -f "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json" ]]; then
+        echo -e "${RED}🗑️ Removing old capslock2hyper.json...${NC}"
+        sudo rm -rf "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json"
+    fi
+    echo -e "${PURPLE}📋 Copying new capslock2hyper.json to ${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json...${NC}"
     cp -r "${TMP_DIR}/capslock2hyper.json" "${HOME}/.config/karabiner/assets/complex_modifications/capslock2hyper.json"
 fi
 
@@ -257,8 +267,8 @@ sudo rm -rf /tmp/Fonts/
 echo -e "${GREEN}✔️ Temporary files removed.${NC}"
 echo -e "${GREEN}✅ Script completed successfully. Files have been successfully copied to the user's home directory.${NC}"
 
-
 # 安装 zsh 插件
 /bin/zsh
 
-rm -rf $HOME/.zcompdump;rm -rf $HOME/.zsh_history;
+rm -rf $HOME/.zcompdump
+rm -rf $HOME/.zsh_history

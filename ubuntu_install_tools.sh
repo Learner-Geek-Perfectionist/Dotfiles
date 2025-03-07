3c472060823e4b8503b4dd9e811ebd097416b7ab
信萌新 <1273730703@qq.com>
2025-03-07 20:02:03 +0800

    Update ubuntu_install_tools.sh

--- ubuntu_install_tools.sh
+++ ubuntu_install_tools.sh
@@ -7,7 +7,7 @@ else
     sudo mkdir -p /etc/apt/keyrings
     sudo wget -qO- https://apt.kitware.com/keys/kitware-archive-latest.asc | sudo gpg --dearmor -o /etc/apt/keyrings/kitware.gpg
     echo "deb [signed-by=/etc/apt/keyrings/kitware.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
-
+    
     # 安装 CMake
     sudo apt update && sudo apt install -y cmake
     print_centered_message "${GREEN} cmake 安装完成 ✅${NC}" "false" "true"
@@ -27,9 +27,9 @@ else
     make "-j$(nproc)" && sudo make install
     # 清理整个项目目录，包括源码和编译目录
     cd ../.. && sudo rm -rf fastfetch
-
+    
     print_centered_message "${GREEN} fastfetch 安装完成 ✅${NC}" "false" "false"
-
+    
 fi
 # =================================结束安装 fastfetch=================================
 
@@ -40,23 +40,23 @@ else
     print_centered_message "${GREEN}开始安装 kitty... ${NC}" "true" "false"
     sudo mkdir -p /opt/kitty && sudo chmod -R a+rw /opt/kitty
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n dest=/opt/kitty
-
+    
     # 定义基础 URL
     BASE_URL="http://kr.archive.ubuntu.com/ubuntu/pool/universe/k/kitty/"
-
+    
     # 使用 curl 获取页面内容，并解析出最新的 kitty-terminfo .deb 文件的版本号
     TERMINFO_LATEST_VERSION=$(curl -s "$BASE_URL" | grep -oP 'href="kitty-terminfo_[^"]*\.deb"' | sed -E 's|.*kitty-terminfo_([^"]*)\.deb.*|\1|' | sort -V | tail -1)
-
+    DOCS_LATEST_VERSION=$TERMINFO_LATEST_VERSION
     # 如果找不到文件，则退出
     if [ -z "$TERMINFO_LATEST_VERSION" ]; then
         echo -e "${RED}Failed to find the kitty-terminfo .deb file.${NC}"
         exit 1
     fi
-
+    
     # 构建完整的 .deb 文件下载 URL（kitty-terminfo）
     TERMINFO_URL="${BASE_URL}kitty-terminfo_${TERMINFO_LATEST_VERSION}.deb"
-    DOCS_URL="${BASE_URL}kitty-docs_${DOCS_LATEST_VERSION}.deb"
-
+    DOC_URL="${BASE_URL}kitty-doc_${DOCS_LATEST_VERSION}.deb"
+    
     # 下载和安装包（kitty-terminfo）
     echo -e "Downloading ${RED}kitty-terminfo${NC} version ${GREEN}${TERMINFO_LATEST_VERSION}${NC}"
     if curl -s -O "$TERMINFO_URL"; then
@@ -71,10 +71,10 @@ else
         echo -e "${RED}Download failed.${NC}"
         exit 1
     fi
-
+    
     # 下载和安装包（kitty-docs）
     echo -e "Downloading ${RED}kitty-docs${NC} version ${GREEN}${DOCS_LATEST_VERSION}${NC}"
-    if curl -s -O "$DOCS_URL"; then
+    if curl -s -O "$DOC_URL"; then
         echo "Installing kitty-docs..."
         if sudo apt install "./kitty-docs_${DOCS_LATEST_VERSION}.deb"; then
             echo -e "${GREEN}kitty-docs_${DOCS_LATEST_VERSION}.deb 安装完成 ${NC}"
@@ -88,7 +88,7 @@ else
     fi
     # 清理下载的文件
     sudo rm -rf "kitty-terminfo_${TERMINFO_LATEST_VERSION}.deb" "kitty-docs_${DOCS_LATEST_VERSION}.deb"
-
+    
     # 检查是否在 WSL2 中运行或在自动化脚本环境中
     if grep -qi microsoft /proc/version || [[ "$AUTO_RUN" == "true" ]]; then
         print_centered_message "${RED}在 WSL2 中或者 Dockerfile 中不需要安装 kitty 桌面图标${NC}" "false" "false"
@@ -107,7 +107,7 @@ else
     # 将 kitty 二进制文件复制到标准的系统路径
     sudo ln -s /opt/kitty/kitty.app/bin/* /usr/bin/
     print_centered_message "${GREEN} kitty 安装完成 ✅${NC}" "false" "true"
-
+    
 fi
 
 # =================================结束安装 kitty=================================
@@ -119,11 +119,11 @@ else
     print_centered_message "${GREEN}开始安装 fzf... ${NC}" "false" "false"
     [[ -d "/tmp/.fzf" ]] && sudo rm -rf "/tmp/.fzf"
     [[ -f "/usr/bin/fzf" ]] && sudo rm -rf "/usr/bin/fzf"
-
+    
     git clone --depth=1 https://github.com/junegunn/fzf.git "/tmp/.fzf"
     yes | /tmp/.fzf/install --no-update-rc
     sudo cp "/tmp/.fzf/bin/fzf" /usr/bin/fzf
-
+    
     # 清理安装目录
     sudo rm -rf "/tmp/.fzf"
     print_centered_message "${GREEN} fzf 安装完成 ✅${NC}" "false" "false"
@@ -138,11 +138,11 @@ if command -v rustc >/dev/null 2>&1; then
 else
     print_centered_message "${GREEN}开始安装 rustc...${NC}" "true" "false"
     # 安装 rustup
-
+    
     # 1. 创建系统级安装目录并设置权限
     sudo mkdir -p /opt/rust/{cargo,rustup}
     sudo chmod -R a+rw /opt/rust/
-
+    
     # 2. 通过 rustup 脚本安装并指定系统目录
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
     # 3. 链接 cargo、rustc、rustup 到系统的PATH 中
@@ -161,7 +161,7 @@ if command -v eza >/dev/null 2>&1; then
     print_centered_message "${GREEN}eza 已安装，跳过安装。${NC}" "false" "false"
 else
     print_centered_message "${GREEN}开始安装 eza... ${NC}" "false" "false"
-
+    
     # 安装 eza
     cargo install eza
     sudo ln -s /opt/rust/cargo/bin/eza /usr/bin/
@@ -219,7 +219,7 @@ else
     cd "${LUA_LATEST_VERSION%.tar.gz}"
     make "-j$(nproc)" && sudo make install
     cd .. && sudo rm -rf "${LUA_LATEST_VERSION%.tar.gz}" "$LUA_LATEST_VERSION"
-
+    
     print_centered_message "${GREEN} lua 安装完成 ✅${NC}" "false" "true"
 fi
 # =================================结束安装 lua=================================

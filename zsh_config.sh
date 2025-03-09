2293b65e7ef057be790cea69af60826647009008
ouyangzhaoxin <1273730703@qq.com>
2025-03-09 18:45:01 +0800

    update

--- zsh_config.sh
+++ zsh_config.sh
@@ -82,7 +82,7 @@ elif [[ $(uname -s) == "Linux" ]]; then
 
         # 解压的依赖工具 xz
         sudo apt install -y xz-utils
-        tools=("zsh" "git" "curl" "make" "g++" "gcc")
+        tools=("zsh" "git" "curl" "make" "g++" "gcc" "wget" "gnupg")
         # 遍历工具列表，检查是否已安装
         for tool in "${tools[@]}"; do
             if ! command -v "$tool" >/dev/null 2>&1; then
